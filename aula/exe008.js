adf4c2dc5dd8fa8cdf08502bb04e57f78c53c2a9
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-09 17:02:48 -0300

    colocando exercicio

---
+++ aula/exe008.js
@@ -0,0 +1,7 @@
+var vel =68.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+//console roda no proprio aparelho ou seja no pc
+if(vel>60){//condição simples!
+    console.log("Você ultrapassaa velocidade permitida,MULTADO!")
+}
+consele.log(`Dirija sempre usando  cinto da segurança!`)
---
+++ aula/exe009.js
@@ -0,0 +1,2 @@
+var pais='Brasil'
+console.log(`vivendo em ${pais}`)
