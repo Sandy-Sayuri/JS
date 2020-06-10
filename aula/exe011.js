de9bb72b9985fb37cd25870d8241a9a9cea3ff72
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-10 07:39:18 -0300

    exercicio 011 completo

---
+++ aula/exe011.js
@@ -0,0 +1,11 @@
+var idade=16
+console.log(`Você tem ${idade} anos.`)
+if(idade<16){
+    console.log('Não volta')
+}
+else if(idade>=16&& idade<18){
+        console.log('Voto opcional')
+    }
+else{
+    console.log('Voto obrigatoriao')
+}
