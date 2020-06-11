b81b3b3be7e4e9cd73c7f25b95b7773eb1a6db11
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-11 08:13:52 -0300

    esses exercicios foram completos

---
+++ aula/exe15.js
@@ -0,0 +1,7 @@
+let num=[5,8,2,9,3]
+// isso é uma lista 
+num.push(1)
+num.sort()
+console.log(num)
+comsole.log(`o vetor tem ${num.length}posições`)
+comsole.log(`O primero valor do vetor é ${num[0]}`)
