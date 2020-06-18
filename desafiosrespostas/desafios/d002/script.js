fc262f4f739260e14bb8d2a6ebd1918e9117dc97
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-17 22:06:00 -0300

    tentativa

---
+++ desafiosrespostas/desafios/d002/script.js
@@ -0,0 +1,5 @@
+function açao(){
+    let nome =window.prompt('qual é o seu nome ?')
+    let idade=window.prompt(`Olá ${nome}! Quantos anos você tem`)
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade`)
+}
