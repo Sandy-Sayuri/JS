52ba7d314d92b8e72161237cde1da7584491c86c
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-17 22:33:18 -0300

    okk

--- desafiosrespostas/desafios/d002/script.js
+++ desafiosrespostas/desafios/d002/script.js
@@ -1,8 +1,5 @@
 function açao(){
     let nome =window.prompt('qual é o seu nome ?')
-    while (nome==null){
-        window.prompt('[ERRO] nome invalido!! \n qual é o seu nome ?')
-    }
     let idade=window.prompt(`Olá ${nome}! Quantos anos você tem`)
     window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade`)
 }
