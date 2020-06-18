59cc7851eca8aff91841bc4920be74b7a2cde745
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-17 23:58:48 -0300

    tudo comcluido

---
+++ desafiosrespostas/desafios/d003/teste.js
@@ -0,0 +1,5 @@
+ function conta(){
+    let numero=prompt('digite um numero qualquer')
+    num=Number.parseInt(numero)
+    window.alert(`Antes de ${num},temos o número ${num-1}\n Depois de ${num}, temos o número ${num+1}`)
+ }
