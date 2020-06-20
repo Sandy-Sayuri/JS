1f6ec39889d8965d55688cbe3bd0cb91b43159aa
Sandy-sayuri <sandysayuri200@gmail.com>
2020-06-20 20:58:49 -0300

    terminado
    des 9

--- desafiosrespostas/desafios/d009/Salario.js
+++ desafiosrespostas/desafios/d009/Salario.js
@@ -1,3 +1,12 @@
 function reajuste(){
-    
+    nome=prompt(`Qual é o nome do funcionário?`)
+    salario=prompt(`Qual é o salário de ${nome}`)
+    sal=Number.parseFloat(salario)
+    porcentagem=prompt(`O salário de ${nome} ai ser reajustado em qual porcentagem?`)
+    por=Number.parseFloat(porcentagem)
+    xx.innerHTML=""
+    res.innerHTML=`<h1> ${nome} recebeu um aumento salarial!</h1>\n`
+    res.innerHTML+=`O salário atual era R$ ${sal}\n`
+    res.innerHTML+=`Com um aumento de ${por}% , o salário vai aumentar R$${sal*(por/100)} no próximo mês\n`
+    res.innerHTML+=`E apartir dai ,${nome} vai passar a ganhar R$ ${sal+(sal*(por/100))}`
 }
